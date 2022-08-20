ce62f86b7f9020484677a727f7591597e3774fc6
Jithin-TChandran <jithin.chandran@auzzio.in>
2022-08-20 16:02:33 +0530

    Update HelloWorld.js

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,4 +1,10 @@
 const http = require('http');
-http.createServer((req,res)=>{
-    res.write("<h1>Jithin T Chandran</h1>");
-}).listen(4000);
+const server = http.createServer((req,res)=>{
+    //res.write("<h1>Jithin T Chandran</h1>");
+    res.setHeader('Content-Type','text/html');
+    res.write('<h1>hello Jithin</h1>');
+    res.end();
+});
+server.listen(3000,'localhost',()=>{
+    console.log('listening for request on 3000')
+});
